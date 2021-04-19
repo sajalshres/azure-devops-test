fb8a668034bb3fab3d4f5264d45688981cd4f09e
sajalshres <sajal.shres@gmail.com>
2021-04-19 07:58:16 +0000

    minor test update

--- services/api/src/index.js
+++ services/api/src/index.js
@@ -2,7 +2,7 @@ const { port, env } = require('./config/variables');
 const logger = require('./config/logger');
 const app = require('./config/app');
 
-// listen to port
+// listen to port and serve app
 app.listen(port, () => logger.info(`API Web Server started on port ${port} (${env})`));
 
 module.exports = app;
