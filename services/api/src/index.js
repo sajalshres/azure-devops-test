098e674fc4f3f90b66059d12a41d66db2d3a991a
sajalshres <sajal.shres@gmail.com>
2021-04-19 07:41:35 +0000

    added comment

--- services/api/src/index.js
+++ services/api/src/index.js
@@ -2,6 +2,7 @@ const { port, env } = require('./config/variables');
 const logger = require('./config/logger');
 const app = require('./config/app');
 
+// listen to port
 app.listen(port, () => logger.info(`API Web Server started on port ${port} (${env})`));
 
 module.exports = app;
